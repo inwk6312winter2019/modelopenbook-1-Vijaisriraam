3ca57cdca7bc7b01a90dc47061697466fa759818
Vijaisriraam <VijaiGopalS@dal.ca>
2019-02-06 14:06:25 -0400

    modified dictionery to have nameif as key & IP addr & subnet as value

--- modelquiz1.py
+++ modelquiz1.py
@@ -1,23 +1,28 @@
-fp=open("running-config.cfg")
-readfile=fp.read()
+import math
+myfile=open("running-config.cfg")
+import math
 
 def list_ifname_ip():
-	mydict=dict()
-	newdict=dict()	
-	for line in readfile.split():
-		word=line.strip()
-		if word not in mydict:
-			mydict[line]=1
-		else:
-			mydict[line]+=1
-#	print(mydict)
+    mydict=dict()
 
-	mylist=[]
-	for key,value in mydict.items():
-		mylist.append((value,key))	
-	print(mylist)
-	
-	for key,value in mydict.items():
+    for line in myfile:
+        if "nameif" in line:
+            mylist = line.split()
+
+            next(myfile)
+            myline = next(myfile)
+            mylist1= myline.split()
+
+            if mylist[0]=='nameif':
+                if mylist1[0] == 'ip':
+                    mytuple=(mylist1[2:])
+                    mydict[mylist[1]]=mytuple
+
+    return mydict
+
+output = list_ifname_ip()
+print(output)
 		
-list_ifname_ip()
-		
+
+
+
